ee1cc58eacaa5cb69f61b4904d6d4fd3af2c7fa1
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2020-04-06 22:44:42 +0200

    pon, 6 kwi 2020, 22:44:42 CEST

--- scenes/livingroommotion.py
+++ scenes/livingroommotion.py
@@ -7,19 +7,21 @@ class Scene(BaseAutomation):
         super().__init__(sid, daemon)
         self.name = 'Livingroom motion'
         self.add_trigger('report.158d0002ec2fa6.status.motion', self.on_motion)
+        self.add_trigger('report.0x0000000007e7bae0.power.on', self.on_power_on)
         self.place = 'Living room'
+        self.lamp = self.get_device('0x0000000007e7bae0')
     
     def on_motion(self):
         sunrise = self.daemon.config['datetime']['sunrise']
         sunset = self.daemon.config['datetime']['sunset']
         if TimeCheck('<>', sunset, sunrise).status:
-            lamp = self.get_device('0x0000000007e7bae0')
-            lamp.on()
-            if TimeCheck('>', '23:00'):
-                lamp.set_bright(1)
-            else:
-                lamp.set_bright(30)
+            self.lamp.on()
         else:
-            lamp = self.get_device('0x0000000007e7bae0')
-            lamp.off()
+            self.lamp.off()
+    
+    def on_power_on(self):
+        if TimeCheck('>', '23:00'):
+            self.lamp.set_bright(1)
+        else:
+            self.lamp.set_bright(30)
             
