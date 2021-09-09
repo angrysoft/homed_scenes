fa41347ae1fcfe3c07bd6008018a7970e190b7e8
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2021-09-09 20:21:52 +0200

    czw, 9 wrz 2021, 20:21:52 CEST

---
+++ scenes/bedroom.py
@@ -0,0 +1,13 @@
+from homedaemon.scenes import BaseAutomation
+
+
+class Scene(BaseAutomation):
+    def __init__(self, sid:str):
+        super().__init__(sid)
+        self.name = 'bedroom motion'
+        self.add_trigger('report.0x00158d0002ec03fe.no_occupancy_since.120', self.on_no_motion)
+
+    def on_no_motion(self):
+        wallsw = self.get_device('0x00158d0002a16338')
+        wallsw.off("left")
+        wallsw.off("right")
