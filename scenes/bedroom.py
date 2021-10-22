a50e5a8dbc0e91104a7397965604262192bf6aca
Seba <sebastian.zwierzchowski@gmail.com>
2021-10-22 20:08:04 +0200

    new time to no motion

--- scenes/bedroom.py
+++ scenes/bedroom.py
@@ -5,7 +5,7 @@ class Scene(BaseAutomation):
     def __init__(self, sid:str):
         super().__init__(sid)
         self.name = 'bedroom motion'
-        self.add_trigger('report.0x00158d0002ec03fe.no_occupancy_since.240', self.on_no_motion)
+        self.add_trigger('report.0x00158d0002ec03fe.no_occupancy_since.900', self.on_no_motion)
 
     def on_no_motion(self):
         wallsw = self.get_device('0x00158d0002a16338')
