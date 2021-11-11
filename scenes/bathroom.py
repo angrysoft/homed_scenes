8b477f148d9019c990bffdda23e4ada01f4dc058
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2021-11-11 22:17:28 +0100

    czw, 11 lis 2021, 22:17:28 CET

--- scenes/bathroom.py
+++ scenes/bathroom.py
@@ -4,25 +4,29 @@ class Scene(BaseAutomation):
     def __init__(self,sid:str):
         super().__init__(sid)
         self.name = 'Bathroom events'
-        # event loop death
         self.add_trigger('report.0x00158d0002abac97.right.ON', self.on_on)
         self.add_trigger('report.0x00158d0002abac97.right.OFF', self.on_off)
         self.add_trigger('report.0x00124b0022ec93cf.occupancy.True', self.on_movement)
         self.add_trigger('report.0x00124b0022ec93cf.occupancy.False', self.on_no_movement)
         self._timer_on = RunAfter(240, self.fun_on)
         self._timer_off = RunAfter(5, self.fun_off)
+        self._timer_on_no_movement = RunAfter(30, self.off_light)
     
     def on_movement(self):
         wallsw = self.get_device('0x00158d0002abac97')
         if wallsw.is_off('right'):
             wallsw.on('right')
+        
+        self._timer_on_no_movement.cancel()
     
     def on_no_movement(self):
+       self._timer_on_no_movement.wait()
+
+    def off_light(self):
         wallsw = self.get_device('0x00158d0002abac97')
         if wallsw.is_on('right'):
             wallsw.off('right')
         
-
     def on_on(self):
         self._timer_off.cancel()
         self._timer_on.wait()
