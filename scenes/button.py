b59b8f4c57700ac26c9fcbfc85ae046317ab919c
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2022-12-18 10:24:59 +0100

    ...

--- scenes/button.py
+++ scenes/button.py
@@ -4,11 +4,12 @@ from homedaemon.scenes import BaseAutomation
 class Scene(BaseAutomation):
     def __init__(self, sid: str):
         super().__init__(sid)
-        self.name = "Button"
-        self.add_trigger("report.0x00124b001f4502db.action.single", self.lamp_tree_toggle)
-        # self.add_trigger("report.0x00124b001f4502db.action.double", self.toggle_bright)
-        self.place = "Living room"
+        self.name = "tree lamp"
+        self.add_trigger("report.0x00158d000200a020.action.single", self.toggle_tree)
+        self.add_trigger("report.0x00124b001f4502db.action.single", self.toggle_tree)
+        # self.add_trigger("report.0x00158d000200a020.action.double", self.toggle_light)
 
-    def lamp_tree_toggle(self):
-        if self.lamp_tree := self.get_device("0x00158d000283b219")
-            self.lamp_tree.toggle()
+    def toggle_tree(self):
+        self.strip_switch = self.get_device("0x00158d000283b219")
+        if self.strip_switch:
+            self.strip_switch.toggle()
